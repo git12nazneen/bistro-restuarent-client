9f7a5c6b12811816e16ac2106df41c81e5e14c68
git12nazneen <lipinazneen65@gmail.com>
2024-05-23 01:28:35 +0600

    added google

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut ,getAuth, onAuthStateChanged, updateProfile} from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut ,getAuth, onAuthStateChanged, updateProfile, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 import { app } from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -9,6 +9,7 @@ const AuthProvider = ({children}) => {
   
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider()
 
     const createUser = (email, password)=>{
         setLoading(true);
@@ -18,6 +19,11 @@ const AuthProvider = ({children}) => {
     const signIn = (email, password)=>{
         setLoading(true)
          return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleSignIn = () =>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
     }
 
     const logOut = () =>{
@@ -48,7 +54,8 @@ const AuthProvider = ({children}) => {
         createUser,
         signIn,
         logOut,
-        updateUserProfile
+        updateUserProfile,
+        googleSignIn
     }
 
 
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -46,7 +46,7 @@ const router = createBrowserRouter([
     },
     {
       path:'dashboard',
-      element:<Dashboard></Dashboard>,
+      element:<PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
       children:[
         {
           path:'cart',
