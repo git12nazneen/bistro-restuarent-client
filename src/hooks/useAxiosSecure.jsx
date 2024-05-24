384f29572225b1c1fe37f0d6a95497b2aff0564f
git12nazneen <lipinazneen65@gmail.com>
2024-05-24 19:40:50 +0600

    added axios secure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,27 @@ import axios from "axios";
 })
 
 const useAxiosSecure = () => {
-    
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token')
+        console.log('request stopped by interceptors', token)
+        config.headers.authorization = `Bearer${token}`
+        return config;
+    },function(error){
+        return Promise.reject(error);
+    })
+
+    // interceptors 401 and 403
+    axiosSecure.interceptors.response.use(function(response){
+        return response;
+    },(error)=>{
+        const status = error.response.status;
+        console.log('status error in the interceptors', status)
+        return Promise.reject(error);
+    })
+
+
+
+
     return axiosSecure;
 };
 
