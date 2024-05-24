60aabd621a5381d2a0e0f74dcc1406ae47403893
git12nazneen <lipinazneen65@gmail.com>
2024-05-24 11:46:36 +0600

    added user:

--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -1,56 +1,71 @@
-import React from 'react';
-import useAxiosSecure from '../../hooks/useAxiosSecure';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 import { MdAdminPanelSettings } from "react-icons/md";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
-    const axiosSecure = useAxiosSecure()
-    const {data: users = [], refetch} = useQuery({
-        queryKey:['users'],
-        queryFn:async()=>{
-            const res = await axiosSecure.get('/users')
-            return res.data;
-        }
-    })
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
 
-    const handleDelete = (user) => {
+  const handleAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`)   
+    .then(res => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
         Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            
-            axiosSecure.delete(`/users/${user.id}`).then((res) => {
-              if (res.data.deletedCount > 0) {
-                refetch()
-                Swal.fire({
-                  title: "Deleted!",
-                  text: "Your file has been deleted.",
-                  icon: "success",
-                });
-              }
+          position: "top-end",
+          icon: "success",
+          title: ` ${user.name} Your are admin now `,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
+
+  const handleDelete = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user.id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
             });
           }
         });
-      };
-    
-
-    return (
-        <div>
-           <div className='flex justify-evenly my-4'>
-            <h2 className='text-3xl'>All users</h2>
-            <h2 className='text-3xl'>Total users: {users.length}</h2>
+      }
+    });
+  };
 
 
-           </div>
+  return (
+    <div>
+      <div className="flex justify-evenly my-4">
+        <h2 className="text-3xl">All users</h2>
+        <h2 className="text-3xl">Total users: {users.length}</h2>
+      </div>
 
-           <div className="overflow-x-auto">
+      <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead>
@@ -79,25 +94,27 @@ const AllUsers = () => {
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        {/* <img
-                          src={item.image}
-                          alt="Avatar Tailwind CSS Component"
-                        /> */}
-                      </div>
+                      <div className="mask mask-squircle w-12 h-12"></div>
                     </div>
-                    <div>
-                      {/* <div className="text-sm opacity-50">United States</div> */}
-                    </div>
+                    <div></div>
                   </div>
                 </td>
                 <td>
                   <div className="font-bold">{user.name}</div>
-                  {/* <br/>
-              <span className="badge badge-ghost badge-sm">Desktop Support Technician</span> */}
                 </td>
                 <td>{user.email}</td>
-                <td><button className='btn-outline font-extrabold'><MdAdminPanelSettings  className='text-3xl'/></button></td>
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={()=>handleAdmin(user)}
+                      className="btn-outline font-extrabold"
+                    >
+                      <MdAdminPanelSettings className="text-3xl" />
+                    </button>
+                  )}
+                </td>
                 <th>
                   <button
                     onClick={() => handleDelete(user._id)}
@@ -112,8 +129,8 @@ const AllUsers = () => {
           {/* foot */}
         </table>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default AllUsers;
