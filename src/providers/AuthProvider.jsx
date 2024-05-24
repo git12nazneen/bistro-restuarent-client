d594566b480d0c251f0947776efc899a6ec7a680
git12nazneen <lipinazneen65@gmail.com>
2024-05-24 14:56:48 +0600

    added token

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut ,getAuth, onAuthStateChanged, updateProfile, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import UseAxiosPublic from '../hooks/UseAxiosPublic';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app)
@@ -10,6 +11,7 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true)
     const googleProvider = new GoogleAuthProvider()
+    const axiosPublic = UseAxiosPublic()
 
     const createUser = (email, password)=>{
         setLoading(true);
@@ -40,6 +42,21 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
      const unsubscribe = onAuthStateChanged (auth,currentUser=>{
         setUser(currentUser)
+        if(currentUser){
+            // get token and store
+            const userInfo = {email: currentUser.email};
+            console.log('user infor', userInfo)
+            axiosPublic.post('/jwt',userInfo)
+            .then(res=>{
+                if(res.data.token){
+                    localStorage.setItem('access-token', res.data.token);
+                }
+            })
+        }
+        else{
+            // remove token .
+            localStorage.removeItem('access-token')
+        }
         console.log('current user', currentUser);
         setLoading(false)
       })
